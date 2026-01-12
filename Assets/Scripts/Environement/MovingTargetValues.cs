98046da6d8ea0759c00b964ae1be8154501c9e02
ChipironWeaver <lowen.tisserand@gmail.com>
2026-01-12 23:14:19 +0100

    12.01.26.d
    the number is not being detected?????
    the animation is being funky

--- Assets/Scripts/Environement/MovingTargetValues.cs
+++ Assets/Scripts/Environement/MovingTargetValues.cs
@@ -1,24 +1,37 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEditor;
 public class MovingTargetValues : MonoBehaviour
 {
     [SerializeField] Transform targetA;
     [SerializeField] Transform targetB;
-    [SerializeField] float Speed;
+    [SerializeField] float speed;
     [SerializeField] float waitTime;
-    float currentTime;
-    private Animator animator;
-    private AnimationClip clip;
+    float _currentTime;
+    private Animation _animation;
 
     private void Start()
     {
-        animator = GetComponent<Animator>();
+        AnimationClip clip = new  AnimationClip();
+        _animation = GetComponent<Animation>();
+        _animation.clip = clip;
         Keyframe[] keys = new Keyframe[4];
-        keys[0] = new Keyframe(0f,0f);
-        keys[1] = new Keyframe(1f,0f);
-        keys[2] = new Keyframe(2f,0f);
-        keys[3] = new Keyframe(3f,0f);
-        //clip.SetCurve("",typeof(Transform), "localPosition", keys);
+        keys[0] = new Keyframe(_currentTime,targetA.position.x); //move to target A
+        _currentTime += speed; //move time
+        keys[1] = new Keyframe(_currentTime,targetA.position.x); //target A
+        _currentTime += waitTime; //wait time
+        keys[2] = new Keyframe(_currentTime,targetB.position.x); //move to target B
+        _currentTime += speed; //move time
+        keys[3] = new Keyframe(_currentTime,targetB.position.x); //target B
+        clip.SetCurve("Platform", typeof(Transform), "position.x", new AnimationCurve(keys));
+        keys[0] = new Keyframe(_currentTime,targetA.position.y); //move to target A
+        _currentTime += speed; //move time
+        keys[1] = new Keyframe(_currentTime,targetA.position.y); //target A
+        _currentTime += waitTime; //wait time
+        keys[2] = new Keyframe(_currentTime,targetB.position.y); //move to target B
+        _currentTime += speed; //move time
+        keys[3] = new Keyframe(_currentTime,targetB.position.y); //target B
+        clip.SetCurve("Platform", typeof(Transform), "position.y", new AnimationCurve(keys));
+        clip.legacy = true;
+        _animation.AddClip(clip, "PlatformAnimation");
+        _animation.Play("PlatformAnimation");
     }
 }
