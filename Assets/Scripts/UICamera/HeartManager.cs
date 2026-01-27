1b11eedb843ef4925502a0a7f3cbfd85502e6cbf
ltisserand-hub <a.tisserand@nouvellesimages.xyz>
2026-01-27 11:49:37 +0100

    [WIP] Heart Instantiation
    The heart manager sript renders hearts in the right position based on the amount and the offset and creates them when needed
    Temporary health isn't being being suported
    The part of the script to switch to the right sprite isn't implemented yet

--- Assets/Scripts/UICamera/HeartManager.cs
+++ Assets/Scripts/UICamera/HeartManager.cs
@@ -1,15 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
 
 public class HeartManager : MonoBehaviour
 {
     [SerializeField] [Range(0, 40)] private int maxHealth; //1 visual heart is considered as 2 HP
     [SerializeField] [Range(0, 40)] int health;
-    [SerializeField] [Range(0, 5)]private int tempHealth; //Temp Health is WIP
+    [SerializeField] [Range(0, 5)] private int tempHealth; //Temp Health is WIP
     [SerializeField] [Range(1, 10)] int heartsPerColumn; //Counts the visual hearts
-    [SerializeField] private Vector2 offSet;
+    [SerializeField] private Vector3 offSet;
     
     [SerializeField] private List<Sprite> hearts;
     //6 images required in this order:
@@ -21,28 +20,30 @@ public class HeartManager : MonoBehaviour
     //5 = Temp Heart
     [SerializeField] public GameObject heartPrefab;
     private Image _image;
-    private List<GameObject> _hearts;
+    private List<GameObject> _hearts = new();
     private Vector3 healthMemory;
-    void Start()
-    {
-        _image = gameObject.GetComponent<Image>();
-        GameObject heart;
-        heart = Instantiate(heartPrefab);
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (new Vector3(health, maxHealth, tempHealth) != healthMemory)
         {
-            /*while (_hearts.Count < maxHealth / 2)
+            while (_hearts.Count < (float)maxHealth / 2)
             {
-                _hearts.Add(Instantiate(heartPrefab, Vector3.zero, Quaternion.identity));
-            }*/
+                GameObject _temp = CreateHeart(_hearts.Count);
+                _hearts.Add(_temp);
+            }
             
             
-            healthMemory = new Vector3(health, maxHealth, tempHealth);
+            
         }
+    }
+
+    GameObject CreateHeart( int _position)
+    {
+        GameObject heart = Instantiate<GameObject>(heartPrefab);
+        heart.transform.SetParent(gameObject.transform,false);
+        heart.GetComponent<RectTransform>().localPosition = new Vector3(offSet.x * (_position % heartsPerColumn + 1),offSet.y*(_position/ heartsPerColumn));
+        heart.name = "Heart " + _position.ToString();
+        return heart;
     }
 }
